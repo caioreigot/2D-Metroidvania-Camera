c38ca06ef918a9c83476b925243022b43e0c0b5c
Caio Costa <caiocrrodrigues2004@gmail.com>
2020-12-10 16:25:29 -0300

    .

--- Assets/Scripts/Camera/FollowPlayer.cs
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -50,9 +50,17 @@ public class FollowPlayer : MonoBehaviour {
 
     void FollowPlayerPos() {
         if (GameObject.Find("Boundary")) {
-            transform.position = new Vector3(Mathf.Clamp(player.position.x, GameObject.Find("Boundary").GetComponent<BoxCollider2D>().bounds.min.x + cameraBox.size.x / 2, GameObject.Find("Boundary").GetComponent<BoxCollider2D>().bounds.max.x - cameraBox.size.x / 2),
-                                 Mathf.Clamp(player.position.y, GameObject.Find("Boundary").GetComponent<BoxCollider2D>().bounds.min.y + cameraBox.size.y / 2, GameObject.Find("Boundary").GetComponent<BoxCollider2D>().bounds.max.y - cameraBox.size.y / 2),
-                                 transform.position.z);
+            transform.position = new Vector3(Mathf.Clamp(
+                // X
+                player.position.x, GameObject.Find("Boundary").GetComponent<BoxCollider2D>().bounds.min.x + cameraBox.size.x / 2, 
+                GameObject.Find("Boundary").GetComponent<BoxCollider2D>().bounds.max.x - cameraBox.size.x / 2),
+                
+                // Y
+                Mathf.Clamp(player.position.y, GameObject.Find("Boundary").GetComponent<BoxCollider2D>().bounds.min.y + cameraBox.size.y / 2, 
+                GameObject.Find("Boundary").GetComponent<BoxCollider2D>().bounds.max.y - cameraBox.size.y / 2),
+                
+                // Z
+                transform.position.z);
         }
     }
 
